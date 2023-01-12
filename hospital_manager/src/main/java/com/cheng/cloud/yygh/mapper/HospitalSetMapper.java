9f2422fe455fcefabf3a5912f7ef91bca0dc6453
Wpengcheng <3194525857@qq.com>
2023-01-12 10:46:05 +0800

    医院接口模拟系统

---
+++ hospital_manager/src/main/java/com/cheng/cloud/yygh/mapper/HospitalSetMapper.java
@@ -0,0 +1,10 @@
+package com.cheng.cloud.yygh.mapper;
+
+import com.cheng.cloud.yygh.model.HospitalSet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HospitalSetMapper extends BaseMapper<HospitalSet> {
+
+}
